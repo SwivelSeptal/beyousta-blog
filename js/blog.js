9b1b187a47f4cf2c85a4696001ccd2bcba7a6d2e
finderfulcrum <finderfulcrum@gmail.com>
2025-04-14 05:17:13 +0000

    particle background

--- js/blog.js
+++ js/blog.js
@@ -211,3 +211,24 @@ magneticElements.forEach(el => {
     el.style.transform = `translate(0px, 0px)`;
   });
 });
+
+// Ripple Effect on Blog Card Click
+document.addEventListener('click', function(e) {
+  const target = e.target.closest('.blog-link');
+  if (!target) return;
+
+  const ripple = document.createElement('span');
+  ripple.className = 'ripple';
+  
+  const rect = target.getBoundingClientRect();
+  const size = Math.max(rect.width, rect.height);
+  ripple.style.width = ripple.style.height = size + 'px';
+  ripple.style.left = (e.clientX - rect.left - size / 2) + 'px';
+  ripple.style.top = (e.clientY - rect.top - size / 2) + 'px';
+  
+  target.appendChild(ripple);
+
+  setTimeout(() => {
+    ripple.remove();
+  }, 600);
+});
